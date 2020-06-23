d6f18d3d8f00b05155c8a8d4a3b829c5a5286a8f
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 18:21:01 +0200

    Add files via upload

---
+++ ArithmaticCoding.java
@@ -0,0 +1,21 @@
+
+package arithmaticcoding;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+
+public class ArithmaticCoding {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArithmaticCodingForm f=new ArithmaticCodingForm();
+      
+        f.setVisible(true);
+        
+    }
+    
+}
